200d96f9cd224dd34c639917b71c0093e6fae1d2
ramyaa <ramyad.solutions@gmail.com>
2026-04-23 00:45:21 +0530

    Add health endpoint and Swagger UI

--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => Results.Ok(new { message = "API is running" }));
 app.MapGet("/hello", () => Results.Ok(new { message = "Hello from AWS API Demo" }));
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }));
 
 app.Run();
 
